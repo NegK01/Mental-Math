57273e4c145dbee5c657038f6b945a0a699464cc
Neg <mauricio.or99@gmail.com>
2026-09-05 22:00:19 -0600

    release: v1.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.negk01.mentalmath"
         minSdk = 26
         targetSdk = 37
-        versionCode = 9
-        versionName = "1.5.0"
+        versionCode = 10
+        versionName = "1.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
